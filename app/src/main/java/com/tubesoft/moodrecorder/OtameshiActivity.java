23f8921ecf2e77ae986ba4771546fa00e276f9c2
tubesoft <inotazo@hotmail.co,jp>
2016-02-29 16:17:31 -0500

    画面遷移の実験のためクラスの追加および変更。

--- app/src/main/java/com/tubesoft/moodrecorder/OtameshiActivity.java
+++ app/src/main/java/com/tubesoft/moodrecorder/OtameshiActivity.java
@@ -1,21 +1,21 @@
 package com.tubesoft.moodrecorder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+import com.tubesoft.moodrecorder.R;
+
+public class OtameshiActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_otameshi);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -29,34 +29,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.mood_recorder, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void onClick(View view){
         switch (view.getId()){
-            case R.id.btnToOtameshi:
-                Intent intent = new Intent(this, OtameshiActivity.class);
+            case R.id.btnToMain:
+                Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
                 break;
         }
     }
+
+
 }
